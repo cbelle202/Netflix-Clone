ddda6b509894034e68961d9511faf93b77856ed6
cbelle202 <93744596+cbelle202@users.noreply.github.com>
2023-05-07 10:38:45 -0400

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Ciara Belle on 5/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
