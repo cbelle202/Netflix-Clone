77a3f5d71ba9475b2cd372e078a7d51cb852f2bc
cbelle202 <93744596+cbelle202@users.noreply.github.com>
2023-05-07 19:41:19 -0400

    fetching tv. capitalize first letter. min 1:48:13

--- Netflix Clone/Models/Movie.swift
+++ Netflix Clone/Models/Movie.swift
@@ -22,27 +22,3 @@ struct Movie: Codable {
     let release_date: String?
     let vote_average: Double
 }
-
-
-/*
- 
- adult = 0;
- "backdrop_path" = "/qmEKe0AEsns2WXinbkGxlCxlTHq.jpg";
- "genre_ids" =             (
-     12,
-     14,
-     35
- );
- id = 493529;
- "media_type" = movie;
- "original_language" = en;
- "original_title" = "Dungeons & Dragons: Honor Among Thieves";
- overview = "A charming thief and a band of unlikely adventurers undertake an epic heist to retrieve a lost relic, but things go dangerously awry when they run afoul of the wrong people.";
- popularity = "1949.123";
- "poster_path" = "/v7UF7ypAqjsFZFdjksjQ7IUpXdn.jpg";
- "release_date" = "2023-03-23";
- title = "Dungeons & Dragons: Honor Among Thieves";
- video = 0;
- "vote_average" = "7.548";
- "vote_count" = 905;
- */
