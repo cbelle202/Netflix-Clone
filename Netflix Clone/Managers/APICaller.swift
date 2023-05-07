bcc61034a77085ee62ea526752ddb55fd5e78b1e
cbelle202 <93744596+cbelle202@users.noreply.github.com>
2023-05-07 18:43:43 -0400

    documentation update. api start

---
+++ Netflix Clone/Managers/APICaller.swift
@@ -0,0 +1,18 @@
+//
+//  APICaller.swift
+//  Netflix Clone
+//
+//  Created by Ciara Belle on 5/7/23.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "e61aa086fb8397861e1e08a6c3251f08"
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    
+}
--- README.txt
+++ README.txt
@@ -3,3 +3,6 @@ https://www.youtube.com/watch?v=KCgYDCKqato
 
 API
 https://www.themoviedb.org/?language=en-US
+
+Example API request:
+https://api.themoviedb.org/3/movie/550?api_key=e61aa086fb8397861e1e08a6c3251f08
